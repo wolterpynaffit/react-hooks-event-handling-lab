ef0aa5ef44cc49f40056bbad025ddc9d41cea13d
Tiffany Pretlow <wolterpynaffit@yahoo.com>
2023-08-03 10:10:31 -0400

    finishe

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
-// Code EyesOnMe Component Here
+import React, { useState } from "react";
+
+function EyesOnMe() {
+  const [focus, setFocus] = useState(false);
+
+  function toggle() {
+    setFocus(!focus);
+  }
+
+  return (
+    <button onClick={toggle} onBlur={() => setFocus(false)} onFocus={() => setFocus(true)}>
+      {focus ? "Good" : "Hey, eyes on me!"}
+    </button>
+  );
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,13 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    function handleChange(event){
+        console.log('Entering password...' + event.target.value)
+    }
     return (
-        <div></div>
+
+        <input type="password" onChange={handleChange}></input>
+
     )
 }
 
